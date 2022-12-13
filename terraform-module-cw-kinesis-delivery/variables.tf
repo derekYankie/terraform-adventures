4454d9851b3f93d766fa7a3413f22bc6a17d0f29
derekYankie <derek.afriyie@gmail.com>
2022-12-13 09:22:29 -0500

    Update Module 💃

--- terraform-module-cw-kinesis-delivery/variables.tf
+++ terraform-module-cw-kinesis-delivery/variables.tf
@@ -2,18 +2,18 @@ variable "prefix" {
   type        = string
   description = "Kinesis delivery stream prefix."
   nullable    = false
-  default     = "dt"
+  default     = "demo"
 }
 
 variable "region" {
   type        = string
-  description = "The AWS region"
+  description = "The AWS region."
   nullable    = false
 }
 
 variable "stage" {
   type        = string
-  description = "ID element. Usually used to indicate role, e.g. 'prod', 'staging', 'source', 'build', 'test', 'deploy', 'release'"
+  description = "ID element. Usually used to indicate: role, e.g. 'prod', 'staging', 'source', 'build', 'test', 'deploy', 'release.'"
   nullable    = false
 }
 
@@ -24,67 +24,67 @@ variable "application" {
 }
 
 variable "aws_account_id" {
-  description = "The AWS account ID where the OIDC provider lives, leave empty to use the account for the AWS provider"
+  description = "The AWS account ID where the OIDC provider lives, (leave empty to use the account for the AWS provider)."
   type        = string
   nullable    = false
 }
 
 variable "destinantion_firehose_endpoint" {
   type        = string
-  description = "Destination HTTP Endpoint URL"
+  description = "Destination HTTP Endpoint URL."
   nullable    = false
 }
 
 variable "http_endpoint_buffering_interval" {
-  description = "Kinesis Firehose buffer interval in seconds"
+  description = "Kinesis Firehose buffer interval (in seconds)."
   type        = number
   default     = 60
 }
 
 variable "http_endpoint_buffering_size" {
-  description = "Kinesis Firehose buffer size in MB"
+  description = "Kinesis Firehose buffer (size in MB)."
   type        = number
   default     = 4
 }
 
 variable "output_format" {
-  description = "CloudWatch metric stream output format"
+  description = "CloudWatch metric stream output format."
   type        = string
   nullable    = false
 }
 
 variable "http_access_key" {
-  description = "This the destination API Key"
+  description = "This the destination API Key."
   type        = string
   nullable    = true
 }
 
 variable "http_endpoint_retry_duration" {
-  description = "Kinesis Firehose stream retry in seconds"
+  description = "Kinesis Firehose stream retry (in seconds)."
   type        = number
   default     = 60
 }
 
 variable "namespace_include_filters" {
-  description = "A list of CloudWatch Metric namespaces to stream metrics. Optional"
+  description = "A list of CloudWatch Metric namespaces to stream metrics. Optional."
   type        = list(string)
   default     = []
 }
 
 variable "namespace_exclude_filters" {
-  description = "A list of CloudWatch Metric namespaces to stream metrics. Optional"
+  description = "A list of CloudWatch Metric namespaces to stream metrics. Optional."
   type        = list(string)
   default     = []
 }
 
 variable "s3_buffer_interval" {
-  description = "S3 buffer interval in seconds"
+  description = "S3 buffer interval (in seconds)."
   type        = number
   default     = 300
 }
 
 variable "s3_buffer_size" {
-  description = "S3 buffer size in MB"
+  description = "S3 buffer (size in MB)."
   type        = number
   default     = 5
 }
@@ -96,7 +96,7 @@ variable "log_group_names" {
 }
 
 variable "kinesis_lg_retention_in_days" {
-  description = "Kinesis log group retention time in days"
+  description = "Kinesis log group retention (time in days)."
   type        = number
   default     = 7
 }
